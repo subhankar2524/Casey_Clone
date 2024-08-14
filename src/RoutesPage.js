cfda26ebc347f2af090d4be7e9a410f3fc8cd74d
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-08-14 12:29:56 +0530

    make routes for the webform

---
+++ src/RoutesPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import App from './App'; 
+import WebForm from './WebForm';
+
+function RoutesPage() {
+  return (
+    <Routes>
+        <Route path='/' element={<App />}/>
+        <Route path='/form' element={<WebForm />}/>
+    </Routes>
+  )
+}
+
+export default RoutesPage
---
+++ src/WebForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WebForm() {
+  return (
+    <div>WebForm here</div>
+  )
+}
+
+export default WebForm
